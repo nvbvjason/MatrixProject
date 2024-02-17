fac66372e3b99bf4937f3d3b9054b4242ef9a5a0
nvbvjason <107922108+nvbvjason@users.noreply.github.com>
2024-02-17 09:46:22 +0100

    main test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
 #include "Matrix.hpp"
+#include "Timer.hpp"
 
 int main()
 {
-    auto* jason = new MyMatrix::Matrix{10, 10, 5};
-    auto* la = new MyMatrix::Matrix{10, 10, 5};
-    auto* bla = new MyMatrix::Matrix{10, 10, 0};
-    *bla = *jason * *la;
-    std::cout << *bla;
+    int32_t size = 1500;
+    auto* jason = new MyMatrix::Matrix{size, size};
+    auto* bla = new MyMatrix::Matrix{size, size};
+    {
+        Timer timer;
+        *jason * *bla;
+    }
 }
